438fe9bc7430c364ef97cda02b57c4616aa2089d
Hüseyin Yıldırım <huseyinyildirim@hotmail.com>
2022-02-12 21:46:51 +0300

    first commit

---
+++ basic-maths-unit-test/BasicMathsUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using basic_maths;
+using Xunit;
+
+namespace basic_maths_unit_test
+{
+    public class BasicMathsUnitTest
+    {
+        [Fact]
+        public void Test_Addition()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Addition(10, 10);
+            Assert.Equal(20, result);
+        }
+
+        [Fact]
+        public void Test_Extraction()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Extraction(20, 10);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void Test_Division()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Division(100, 10);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void Test_Multiplication()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.Multiplication(10, 10);
+            Assert.Equal(100, result);
+        }
+    }
+}
---
+++ unit-test-example/basic-maths/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basic_maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
